74957bd3fd3c2b68a01ab4b5c40c82455051662e
Artur420Art <art.gasparyan.420@gmail.com>
2023-05-04 16:41:29 +0400

    Member.Context

---
+++ Member.Context/MemberContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Member.DOMAIN.Entity;
+using MySql.Data.MySqlClient;
+using MySql.Data.EntityFramework;
+using MySql;
+using MySqlX.XDevAPI;
+using System.Configuration;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Microsoft.Extensions.Options;
+
+namespace Member.Context
+{
+
+	public class MemberContext : DbContext
+	{
+
+		public DbSet<MemberEntity> Users { get; set; }
+
+
+		public MemberContext() : base()
+		{
+
+		}
+
+		public MemberContext(DbContextOptionsBuilder<MemberContext> options)
+			: base()
+		{
+			OnConfiguring(options);
+
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+            options.UseMySql("server=localhost;database=Members;user=root;password=Artur2023art", new MySqlServerVersion(new Version(8, 0, 23)));
+
+			base.OnConfiguring(options);
+		}
+		
+
+	}
+}
+
+
---
+++ Member.Context/Migrations/20230504092454_1.0.0.0.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Member.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1000 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    surname = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    phonenumber = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    email = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    password = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
